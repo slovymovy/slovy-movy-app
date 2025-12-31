3f7426af8a1d20880192fd641914c7ba55076f39
nkey <mihailnikalayeu@gmail.com>
2025-12-31 15:59:04 +0100

    chore: remove `COLLATE NOCASE` from indexes and queries, bump database version to v7
    - Simplified index definitions and query conditions in `Dictionary.sq` by removing `COLLATE NOCASE`.
    - Updated `DataDbManager` database version to `v7` to reflect schema changes.
    - Regenerated test database files for alignment with the new schema.

--- composeApp/src/commonMain/kotlin/com/slovy/slovymovyapp/data/remote/DataDbManager.kt
+++ composeApp/src/commonMain/kotlin/com/slovy/slovymovyapp/data/remote/DataDbManager.kt
@@ -37,7 +37,7 @@ class DataDbManager(
     internal val databaseCache = ReadOnlyDatabaseCache(platform)
 
     companion object {
-        const val VERSION = "v6"
+        const val VERSION = "v7"
 
         private const val DICTIONARY_PREFIX = "dictionary_"
         private const val TRANSLATION_PREFIX = "translation_"
