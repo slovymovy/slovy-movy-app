d4abd44ba12ffb40d75d6147359d2c82d3842bfb
nkey <mihailnikalayeu@gmail.com>
2026-01-04 12:40:13 +0100

    test: use `runBlocking` in `SettingsRepositoryServerTest` to validate coroutine support

--- server/src/test/kotlin/com/slovy/slovymovyapp/SettingsRepositoryServerTest.kt
+++ server/src/test/kotlin/com/slovy/slovymovyapp/SettingsRepositoryServerTest.kt
@@ -4,6 +4,7 @@ import com.slovy.slovymovyapp.builder.ServerDbManager
 import com.slovy.slovymovyapp.data.settings.Setting
 import com.slovy.slovymovyapp.data.settings.SettingsRepository
 import com.slovy.slovymovyapp.db.AppDatabase
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import java.nio.file.Files
 import kotlin.test.Test
@@ -11,7 +12,7 @@ import kotlin.test.assertEquals
 
 class SettingsRepositoryServerTest {
     @Test
-    fun repository_works_on_server_with_in_memory_db() {
+    fun repository_works_on_server_with_in_memory_db() = runBlocking {
         val db: AppDatabase = ServerDbManager(Files.createTempDirectory("tmpDirPrefix").toFile()).openApp()
         val repo = SettingsRepository(db)
         val setting = Setting(Setting.Name.TEST_PROPERTY, Json.parseToJsonElement("{\"port\": 8080}"))
