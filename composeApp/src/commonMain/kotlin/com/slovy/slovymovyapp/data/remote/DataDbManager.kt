05b224cc5fe833430afecd16c51367f11648aec3
nkey <mihailnikalayeu@gmail.com>
2026-01-24 11:25:29 +0100

    bump: update database version to v8 in `DataDbManager`

--- composeApp/src/commonMain/kotlin/com/slovy/slovymovyapp/data/remote/DataDbManager.kt
+++ composeApp/src/commonMain/kotlin/com/slovy/slovymovyapp/data/remote/DataDbManager.kt
@@ -37,7 +37,7 @@ class DataDbManager(
     internal val databaseCache = ReadOnlyDatabaseCache(platform)
 
     companion object {
-        const val VERSION = "v7"
+        const val VERSION = "v8"
 
         private const val DICTIONARY_PREFIX = "dictionary_"
         private const val TRANSLATION_PREFIX = "translation_"
