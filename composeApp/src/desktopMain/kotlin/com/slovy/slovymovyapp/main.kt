f7a62612ddc57d686a3100e089dde1ed19113fd1
nkey <mihailnikalayeu@gmail.com>
2026-04-10 01:01:13 +0200

    bump: update versionCode to 17 across platforms

--- composeApp/src/desktopMain/kotlin/com/slovy/slovymovyapp/main.kt
+++ composeApp/src/desktopMain/kotlin/com/slovy/slovymovyapp/main.kt
@@ -10,7 +10,7 @@ import com.slovy.slovymovyapp.data.settings.SettingsRepository
 fun main() = application {
     val buildConfig = AppBuildConfig(
         versionName = "1.0.0",
-        versionCode = 16, // version
+        versionCode = 17, // version
         isDebug = false,
         applicationId = "com.slovy.slovymovyapp"
     )
